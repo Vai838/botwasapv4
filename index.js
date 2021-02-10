ee5915f6f0abfc1edf0f6490bfecf685ae1ba3a3
Vai838 <75434115+Vai838@users.noreply.github.com>
2021-02-10 08:41:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ function kyun(seconds){
   var seconds = Math.floor(seconds % 60);
 
   
-  return `${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik`
+  return `${pad(hours)} Hours ${pad(minutes)} Minutes ${pad(seconds)} Seconds`
 }
 /********** FUNCTION ***************/
 
